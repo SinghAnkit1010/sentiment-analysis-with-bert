7e12ea78aae05ddf924a5a45cb00a270bd4ec7da
Ankit kumar <ankit67.ugcs20@iiitranchi.ac.in>
2023-01-11 00:05:53 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,67 @@
+import numpy as np
+import pandas as pd
+import re
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_text as text
+from flask import Flask,render_template,request
+
+preprocess_layer = hub.KerasLayer(r".\bert_en_uncased_preprocess_3")
+bert_layer = hub.KerasLayer(r".\small_bert_bert_en_uncased_L-6_H-256_A-4_2",trainable = False)
+
+text_input = tf.keras.layers.Input(shape = (),dtype = tf.string,name = "text")
+text_preprocessed = preprocess_layer (text_input)
+outputs = bert_layer(text_preprocessed)
+net = tf.keras.layers.Dropout(0.2)(outputs["pooled_output"])
+output = tf.keras.layers.Dense(units =1,activation = "sigmoid", name = "output")(net)
+model = tf.keras.Model(inputs = [text_input],outputs = output)
+
+model.load_weights("./Bert_model.h5")
+
+class Sentiment:
+  def data_clean(self,sentence):
+    list = []
+    for text in sentence:
+      string = re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z\t])|(\w+:\/\/\S+)"," ",text).lower().split()
+      string = " ".join(string)
+      list.append(string)
+    return list
+  def result(self,sentence):
+    data = self.data_clean(sentence)
+    Result = model.predict(data)
+    score = []
+    sentiment = []
+    prediction = []
+    for i in range(len(sentence)):
+      dict = {}
+      value = Result[i][0]
+      if(value>0.6):
+        sentiment.append("positive")
+      elif(value>0.4):
+        sentiment.append("neutral")
+      else:
+        sentiment.append("negative")
+      score.append(value)
+      dict["setences"] = sentence[i]
+      dict["score"] = score[i]
+      dict["sentiment"] = sentiment[i]
+      prediction.append(dict)
+    return prediction
+obj = Sentiment()
+app = Flask(__name__,template_folder='template')
+
+@app.route("/")
+def index():
+  return render_template("index.html")
+
+@app.route("/predict", methods = ["GET","POST"])
+def predict():
+   if request.method == 'POST':
+    uploaded_file = request.files['uploaded_file']
+    df = pd.read_csv(uploaded_file,"\t")
+    sentences = list(df.iloc[:,0].values)
+    predict = obj.result(sentences)
+    return render_template("index.html",prediction = predict)
+
+if __name__ == "__main__":
+  app.run(debug=True)
